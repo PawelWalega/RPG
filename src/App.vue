22ac3fb6c582c541e12f1b8ec29e754420917d42
Pawel <pawel.walega@protonmail.com>
2020-07-27 19:49:41 +0200

    Bootstrap 5 Installedgit add .

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col">1 of 2</div>
+        <div class="col">2 of 2</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
